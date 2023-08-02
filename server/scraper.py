cd97309d5e09b1b17f18f3618916507d0bee0eb3
Zachary <zacharykangjh@gmail.com>
2023-08-02 22:35:35 +0800

    Randomise User-Agent for each GET request

--- server/scraper.py
+++ server/scraper.py
@@ -1,13 +1,29 @@
 """Defines Carousell scraper functions."""
 
 import re
+import random
 from bs4 import BeautifulSoup
 import requests
+
+USER_AGENTS_LIST = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/91.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/93.0.961.47 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36",
+]
 
 
 def scrape_latest_listings(url, number_of_listings=5):
     """Scrapes and returns the listing data of the latest listings from the specified URL."""
-    response = requests.get(url, timeout=3)
+    response = requests.get(
+        url, headers={"User-Agent": random.choice(USER_AGENTS_LIST)}, timeout=3
+    )
 
     soup = BeautifulSoup(response.text, "lxml")
 
