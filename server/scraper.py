4cd4530978de50af9951d67003fe73a3c8a143d2
Zachary <zacharykangjh@gmail.com>
2023-08-17 15:36:34 +0800

    Update scraper logic to work with updated Carousell website

--- server/scraper.py
+++ server/scraper.py
@@ -34,20 +34,21 @@ def scrape_latest_listings(url, number_of_listings=5):
     latest_listing_data = []
 
     for listing_card in listing_cards[0:number_of_listings]:
+        listing_id = listing_card["data-testid"].split("-")[-1]
+        url = f"https://www.carousell.sg/p/{listing_id}/"
+
         p_tags = listing_card.find_all("p")
 
         p_tags_data = list(map(lambda p_tag: p_tag.string, p_tags))
 
-        if p_tags_data[2] != "Protection":
-            p_tags_data.insert(2, "No Protection")
+        if p_tags_data[2] != "Buyer Protection":
+            p_tags_data.insert(2, "No Buyer Protection")
 
         a_tags = listing_card.find_all("a")
 
         a_tags_data = list(
             map(lambda a_tag: f"https://www.carousell.sg{a_tag['href']}", a_tags)
         )
-
-        url = get_simple_listing_url(a_tags_data[1])
 
         listing_data = {
             "username": p_tags_data[0],
@@ -63,18 +64,3 @@ def scrape_latest_listings(url, number_of_listings=5):
         latest_listing_data.append(listing_data)
 
     return latest_listing_data
-
-
-def get_simple_listing_url(original_url):
-    """Simplfies a Carousell listing URL by using only the listing ID."""
-    original_url_reversed = original_url[::-1]
-
-    listing_id = ""
-    for char in original_url_reversed:
-        if char == "-":
-            break
-
-        if char.isdigit():
-            listing_id = char + listing_id
-
-    return f"https://www.carousell.sg/p/{listing_id}/"
